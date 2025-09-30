5d297fb12d38cacc883e0c38240e1f577f2a8484
Leo <leozinho17silvacosta@gmail.com>
2025-09-29 17:58:05 -0700

    Complete exercise 01

--- ex09.js
+++ ex09.js
@@ -3,4 +3,8 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
+const sortByProperty = {arr, }
+
+
+
 console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
