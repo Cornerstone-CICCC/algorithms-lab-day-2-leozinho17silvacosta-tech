9dd3bdad0c38ee0b2e13826966d3fe008ef83b58
Leo <leozinho17silvacosta@gmail.com>
2025-09-29 18:37:36 -0700

    Complete exercise 04

--- ex04.js
+++ ex04.js
@@ -2,6 +2,24 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
-const mostFrequentChar = 
+const mostFrequentChar = (str) => {
+    const frequent = {};
+
+    for (const char of str) {
+        frequent[char] = (frequent[char] || 0) + 1;
+    }
+
+    let maximumChar = "";
+    let maximumCount = 0;
+
+    for (const char in frequent) {
+        if (frequent[char] > maximumCount) {
+        maximumCount = frequent[char];
+        maximumChar = char;
+    }
+  }
+    
+   return maximumChar;
+};
 
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
