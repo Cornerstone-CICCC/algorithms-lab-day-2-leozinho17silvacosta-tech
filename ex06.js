010c4b0d0f938c523f726bba1fad0867acaf2b59
Leo <leozinho17silvacosta@gmail.com>
2025-09-29 19:23:47 -0700

    Complete exercise 06

--- ex06.js
+++ ex06.js
@@ -3,9 +3,21 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
-const getQueryParams = (URL) => {
+const getQueryParams = (url) => {
     const parametersObject = {};
+
+    const queryString = url.split("?")[1];
+    if (!queryString)
+        return parametersObject;
+
+    const pairs = queryString.split("&");
     
-}
+    for (let pair of pairs) {
+        const [key, value] = pair.split("=");
+        parametersObject[key] = value;
+    }
+
+    return parametersObject;
+};
 
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
